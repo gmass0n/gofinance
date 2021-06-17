44c71b196b6989d2c58e3e5198ba337da9863fbb
Gabriel Masson <g.masson010@gmail.com>
2021-06-17 02:09:50 -0300

    feat: Adds initial project structure and adds styled components theme

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+export const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Text>Hello Dashboard</Text>
+    </Container>
+  );
+};

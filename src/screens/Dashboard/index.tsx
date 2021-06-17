1641be14fac30ec8a07eda03b506967ff37bb1ef
Gabriel Masson <g.masson010@gmail.com>
2021-06-17 08:44:24 -0300

    feat: Adds header componenent to dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  PowerIcon,
+} from "./styles";
 
 export const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Title>Hello Dashboard</Title>
+      <Header>
+        <UserInfo>
+          <Photo source={{ uri: "https://github.com/gmass0n.png" }} />
+
+          <User>
+            <UserGretting>Olá, </UserGretting>
+
+            <UserName>Gabriel</UserName>
+          </User>
+        </UserInfo>
+
+        <TouchableOpacity activeOpacity={0.5}>
+          <PowerIcon />
+        </TouchableOpacity>
+      </Header>
     </Container>
   );
 };
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,55 @@
 import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
 
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-  font-size: 26px;
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  padding: 0 25px;
 
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+  flex: 1;
+`;
+
+export const Photo = styled.Image`
+  width: ${RFValue(50)}px;
+  height: ${RFValue(50)}px;
+  border-radius: 10px;
+  margin-right: 15px;
+`;
+
+export const User = styled.View``;
+
+export const UserGretting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(17)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(17)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const PowerIcon = styled(Feather).attrs({
+  name: "power",
+})`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(22)}px;
 `;
